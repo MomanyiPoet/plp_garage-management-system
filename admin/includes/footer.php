fef565041f07086edced37154c1326fc7532ef42
Momanyi Poet <momanyiturner@gmail.com>
2023-06-21 11:44:49 +0300

    updated website details

---
+++ admin/includes/footer.php
@@ -0,0 +1,5 @@
+<!--footer-->
+    <div class="footer">
+       <p>&copy; 2023 UTAWALA SERVICE GARAGE MANAGEMENT SYSTEM</p>
+    </div>
+        <!--//footer-->
--- bpms/admin/includes/footer.php
+++
@@ -1,5 +0,0 @@
-<!--footer-->
-    <div class="footer">
-       <p>&copy; 2022 UTAWALA SERVICE GARAGE MANAGEMENT SYSTEM</p>
-    </div>
-        <!--//footer-->
