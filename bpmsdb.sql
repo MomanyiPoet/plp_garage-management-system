a5b71279479b70a6b87ac1b9d65c3f24d5edd033
Momanyi Poet <momanyiturner@gmail.com>
2023-06-29 16:57:53 +0300

    added DB

---
+++ bpmsdb.sql
@@ -0,0 +1,294 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 29, 2023 at 03:56 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bpmsdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `ID` int(10) NOT NULL,
+  `AdminName` char(50) DEFAULT NULL,
+  `UserName` char(50) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `Password` varchar(200) DEFAULT NULL,
+  `AdminRegdate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tbladmin`
+--
+
+INSERT INTO `tbladmin` (`ID`, `AdminName`, `UserName`, `MobileNumber`, `Email`, `Password`, `AdminRegdate`) VALUES
+(1, 'Admin', 'admin', 7898799798, 'tester1@gmail.com', '0192023a7bbd73250516f069df18b500', '2022-05-25 06:21:50');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblbook`
+--
+
+CREATE TABLE `tblbook` (
+  `ID` int(10) NOT NULL,
+  `UserID` int(10) DEFAULT NULL,
+  `AptNumber` int(10) DEFAULT NULL,
+  `AptDate` date DEFAULT NULL,
+  `AptTime` time DEFAULT NULL,
+  `Message` mediumtext DEFAULT NULL,
+  `BookingDate` timestamp NULL DEFAULT current_timestamp(),
+  `Remark` varchar(250) DEFAULT NULL,
+  `Status` varchar(250) DEFAULT NULL,
+  `RemarkDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblbook`
+--
+
+INSERT INTO `tblbook` (`ID`, `UserID`, `AptNumber`, `AptDate`, `AptTime`, `Message`, `BookingDate`, `Remark`, `Status`, `RemarkDate`) VALUES
+(10, 12, 706446201, '2023-06-20', '12:00:00', 'Diagnostic Services', '2023-06-20 08:51:55', 'Appointment accepted', 'Selected', '2023-06-20 08:53:12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcontact`
+--
+
+CREATE TABLE `tblcontact` (
+  `ID` int(10) NOT NULL,
+  `FirstName` varchar(200) DEFAULT NULL,
+  `LastName` varchar(200) DEFAULT NULL,
+  `Phone` bigint(10) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `Message` mediumtext DEFAULT NULL,
+  `EnquiryDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `IsRead` int(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblinvoice`
+--
+
+CREATE TABLE `tblinvoice` (
+  `id` int(11) NOT NULL,
+  `Userid` int(11) DEFAULT NULL,
+  `ServiceId` int(11) DEFAULT NULL,
+  `BillingId` int(11) DEFAULT NULL,
+  `PostingDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblinvoice`
+--
+
+INSERT INTO `tblinvoice` (`id`, `Userid`, `ServiceId`, `BillingId`, `PostingDate`) VALUES
+(4, 7, 4, 138889283, '2022-05-13 11:42:21'),
+(5, 7, 9, 138889283, '2022-05-13 11:42:21'),
+(6, 7, 16, 138889283, '2022-05-13 11:42:21'),
+(7, 8, 3, 555850701, '2022-05-13 11:42:51'),
+(8, 8, 6, 555850701, '2022-05-13 11:42:51'),
+(9, 8, 9, 555850701, '2022-05-13 11:42:51'),
+(10, 8, 11, 555850701, '2022-05-13 11:42:51'),
+(13, 10, 1, 330026346, '2022-05-28 08:51:42'),
+(14, 10, 2, 330026346, '2022-05-28 08:51:42'),
+(15, 11, 2, 379060040, '2022-05-29 07:36:17'),
+(16, 11, 5, 379060040, '2022-05-29 07:36:18'),
+(17, 11, 6, 379060040, '2022-05-29 07:36:18'),
+(18, 11, 12, 379060040, '2022-05-29 07:36:18'),
+(19, 11, 3, 460087279, '2022-06-01 01:03:58'),
+(20, 12, 19, 202168997, '2023-06-20 08:59:20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblpage`
+--
+
+CREATE TABLE `tblpage` (
+  `ID` int(10) NOT NULL,
+  `PageType` varchar(200) DEFAULT NULL,
+  `PageTitle` mediumtext DEFAULT NULL,
+  `PageDescription` mediumtext DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `UpdationDate` date DEFAULT NULL,
+  `Timing` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblpage`
+--
+
+INSERT INTO `tblpage` (`ID`, `PageType`, `PageTitle`, `PageDescription`, `Email`, `MobileNumber`, `UpdationDate`, `Timing`) VALUES
+(1, 'aboutus', 'About Us', '        <p class=\"MsoNormal\">Whether you require routine maintenance, repairs, or\r\nspecialized services, our dedicated professionals are here to ensure your\r\nvehicle performs at its best. We understand the importance of your vehicle in\r\nyour daily life, and our goal is to keep you on the road safely and efficiently.<o:p></o:p></p>', NULL, NULL, NULL, ''),
+(2, 'contactus', 'Contact Us', '        Kincar, Utawala, Nairobi Kenya', 'momanyiturner@gmail.com', 254700013897, NULL, '8:30 am to 7:30 pm');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblservices`
+--
+
+CREATE TABLE `tblservices` (
+  `ID` int(10) NOT NULL,
+  `ServiceName` varchar(200) DEFAULT NULL,
+  `ServiceDescription` mediumtext DEFAULT NULL,
+  `Cost` int(10) DEFAULT NULL,
+  `Image` varchar(200) DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblservices`
+--
+
+INSERT INTO `tblservices` (`ID`, `ServiceName`, `ServiceDescription`, `Cost`, `Image`, `CreationDate`) VALUES
+(18, 'Maintenance and Tune-ups', 'From oil changes to tire rotations, we offer comprehensive maintenance services to keep your vehicle running smoothly.', 3000, 'e5d67599bbb698e0cb7ec7a0713419e21687248979.jpg', '2023-06-20 08:16:19'),
+(19, 'Diagnostic Services', 'Our advanced diagnostic tools enable us to identify and resolve any issues your vehicle may be experiencing accurately', 3500, '88657803b1e7af787dc8df3c6c0344511687249057.jpg', '2023-06-20 08:17:37'),
+(20, 'Brake and Suspension', 'Trust us to keep your brakes responsive and your suspension system in optimal condition, ensuring a smooth and safe ride.', 4000, '588b11ad7a92d13777fe0be3adf633bf1687249144.jpg', '2023-06-20 08:19:04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbluser`
+--
+
+CREATE TABLE `tbluser` (
+  `ID` int(10) NOT NULL,
+  `FirstName` varchar(120) DEFAULT NULL,
+  `LastName` varchar(250) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(120) DEFAULT NULL,
+  `Password` varchar(120) DEFAULT NULL,
+  `RegDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tbluser`
+--
+
+INSERT INTO `tbluser` (`ID`, `FirstName`, `LastName`, `MobileNumber`, `Email`, `Password`, `RegDate`) VALUES
+(12, 'Brian', 'Momanyi', 727241497, 'momanyi@gmail.com', '759e97961bfac5d256f15e8217577d28', '2023-06-20 08:49:59');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblbook`
+--
+ALTER TABLE `tblbook`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblcontact`
+--
+ALTER TABLE `tblcontact`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblinvoice`
+--
+ALTER TABLE `tblinvoice`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
+
+--
+-- Indexes for table `tblpage`
+--
+ALTER TABLE `tblpage`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblservices`
+--
+ALTER TABLE `tblservices`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tbluser`
+--
+ALTER TABLE `tbluser`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `ID` (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tblbook`
+--
+ALTER TABLE `tblbook`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `tblcontact`
+--
+ALTER TABLE `tblcontact`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tblinvoice`
+--
+ALTER TABLE `tblinvoice`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `tblpage`
+--
+ALTER TABLE `tblpage`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblservices`
+--
+ALTER TABLE `tblservices`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `tbluser`
+--
+ALTER TABLE `tbluser`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
