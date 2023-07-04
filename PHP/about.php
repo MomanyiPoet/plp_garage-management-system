414360dceb3142a10759b2ab3b39d66ed6c34d65
Momanyi Poet <momanyiturner@gmail.com>
2023-07-04 15:54:43 +0300

    added more pages

--- PHP/about.php
+++ PHP/about.php
@@ -5,8 +5,9 @@ include('includes/dbconnection.php');
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
-    
+
+<head>
+
     <title>UTAWALA SERVICE GARAGE MANAGEMENT SYSTEM</title>
 
     <!-- Template CSS -->
@@ -14,143 +15,146 @@ include('includes/dbconnection.php');
     <link href="https://fonts.googleapis.com/css?family=Josefin+Slab:400,700,700i&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
-  </head>
-  <body id="home">
-<?php include_once('includes/header.php');?>
+</head>
 
-<script src="assets/js/jquery-3.3.1.min.js"></script> <!-- Common jquery plugin -->
-<!--bootstrap working-->
-<script src="assets/js/bootstrap.min.js"></script>
-<!-- //bootstrap working-->
-<!-- disable body scroll which navbar is in active -->
-<script>
-$(function () {
-  $('.navbar-toggler').click(function () {
-    $('body').toggleClass('noscroll');
-  })
-});
-</script>
-<!-- disable body scroll which navbar is in active -->
+<body id="home">
+    <?php include_once('includes/header.php'); ?>
 
-<!-- eadcrbrumbs -->
+    <script src="assets/js/jquery-3.3.1.min.js"></script> <!-- Common jquery plugin -->
+    <!--bootstrap working-->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- //bootstrap working-->
+    <!-- disable body scroll which navbar is in active -->
+    <script>
+        $(function() {
+            $('.navbar-toggler').click(function() {
+                $('body').toggleClass('noscroll');
+            })
+        });
+    </script>
+    <!-- disable body scroll which navbar is in active -->
+
+    <!-- eadcrbrumbs -->
     <section class="w3l-inner-banner-main">
         <div class="about-inner about ">
-            <div class="container">   
+            <div class="container">
                 <div class="main-titles-head text-center">
-                <h3 class="header-name ">
-					About Us
-                </h3>
-            </div>
-</div>
-   </div>
-   <div class="breadcrumbs-sub">
-   <div class="container">   
-    <ul class="breadcrumbs-custom-path">
-        <li class="right-side propClone"><a href="index.php" class="">Home <span class="fa fa-angle-right" aria-hidden="true"></span></a> <p></li>
-        <li class="active ">About</li>
-    </ul>
-</div>
-</div>
-        </div>
-    </section>
-<!-- breadcrumbs //-->
-<section class="w3l-content-with-photo-4"  id="about">
-    <div class="content-with-photo4-block ">
-        <div class="container">
-            <div class="cwp4-two row">
-            <div class="cwp4-image col-xl-6">
-                <img src="assets/images/garage1.jpg" alt="product" class="img-responsive about-me">
-            </div>
-                <div class="cwp4-text col-xl-6 ">
-                    <div class="posivtion-grid">
-                    <h3 class="">Garage and success starts here</h3>
-                    <div class="hair-two-colums">
-                        <div class="hair-left">
-<h5>
-Bodywork and Paint</h5>
-</div>
-                                                                                <div class="hair-left">
-                                                                                    <h5>Air Conditioning and Heating</h5>
-                                    
-                                                                                </div>
-                                                                                 <div class="hair-left">
-                                                                                    <h5>Engine and Transmission</h5>
-                                                                                </div>
-
-   <div class="hair-left">
-                                                                                    <h5>Electrical and Electronics</h5>
-                                                                                </div>
-                                                                                   <div class="hair-left">
-                                                                                    <h5>Brake and Suspension</h5>
-                                                                                </div>
-
-                                                                                   <div class="hair-left">
-                                                                                    <h5>Diagnostic Services</h5>
-                                                                                </div>
-
-                    
+                    <h3 class="header-name ">
+                        About Us
+                    </h3>
                 </div>
             </div>
         </div>
+        <div class="breadcrumbs-sub">
+            <div class="container">
+                <ul class="breadcrumbs-custom-path">
+                    <li class="right-side propClone"><a href="index.php" class="">Home <span class="fa fa-angle-right" aria-hidden="true"></span></a>
+                        <p>
+                    </li>
+                    <li class="active ">About</li>
+                </ul>
+            </div>
         </div>
-    </div>
-</div>
-</section>
+        </div>
+    </section>
+    <!-- breadcrumbs //-->
+    <section class="w3l-content-with-photo-4" id="about">
+        <div class="content-with-photo4-block ">
+            <div class="container">
+                <div class="cwp4-two row">
+                    <div class="cwp4-image col-xl-6">
+                        <img src="assets/images/garage1.jpg" alt="product" class="img-responsive about-me">
+                    </div>
+                    <div class="cwp4-text col-xl-6 ">
+                        <div class="posivtion-grid">
+                            <h3 class="">Garage and success starts here</h3>
+                            <div class="hair-two-colums">
+                                <div class="hair-left">
+                                    <h5>
+                                        Bodywork and Paint</h5>
+                                </div>
+                                <div class="hair-left">
+                                    <h5>Air Conditioning and Heating</h5>
 
-<section class="w3l-recent-work">
-	<div class="jst-two-col">
-		<div class="container">
-<div class="row">
-		<div class="my-bio col-lg-6">
+                                </div>
+                                <div class="hair-left">
+                                    <h5>Engine and Transmission</h5>
+                                </div>
 
-	<div class="hair-make">
-		<?php
+                                <div class="hair-left">
+                                    <h5>Electrical and Electronics</h5>
+                                </div>
+                                <div class="hair-left">
+                                    <h5>Brake and Suspension</h5>
+                                </div>
 
-$ret=mysqli_query($con,"select * from tblpage where PageType='aboutus' ");
-$cnt=1;
-while ($row=mysqli_fetch_array($ret)) {
+                                <div class="hair-left">
+                                    <h5>Diagnostic Services</h5>
+                                </div>
 
-?>
-		<h5><a href="blog.html"><?php  echo $row['PageTitle'];?></a></h5>
-		<p class="para mt-2"><?php  echo $row['PageDescription'];?></p><?php } ?>
-	</div>
-	
-	
-	</div>
-	<div class="col-lg-6 ">
-		<img src="assets/images/about-us.png" alt="product" class="img-responsive about-me">
-	</div>
 
-</div>
-		</div>
-	</div>
-</section>
-<?php include_once('includes/footer.php');?>
-<!-- move top -->
-<button onclick="topFunction()" id="movetop" title="Go to top">
-	<span class="fa fa-long-arrow-up"></span>
-</button>
-<script>
-	// When the user scrolls down 20px from the top of the document, show the button
-	window.onscroll = function () {
-		scrollFunction()
-	};
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
-	function scrollFunction() {
-		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-			document.getElementById("movetop").style.display = "block";
-		} else {
-			document.getElementById("movetop").style.display = "none";
-		}
-	}
+    <section class="w3l-recent-work">
+        <div class="jst-two-col">
+            <div class="container">
+                <div class="row">
+                    <div class="my-bio col-lg-6">
 
-	// When the user clicks on the button, scroll to the top of the document
-	function topFunction() {
-		document.body.scrollTop = 0;
-		document.documentElement.scrollTop = 0;
-	}
-</script>
-<!-- /move top -->
+                        <div class="hair-make">
+                            <?php
+
+                            $ret = mysqli_query($con, "select * from tblpage where PageType='aboutus' ");
+                            $cnt = 1;
+                            while ($row = mysqli_fetch_array($ret)) {
+
+                            ?>
+                                <h5><a href="blog.html"><?php echo $row['PageTitle']; ?></a></h5>
+                                <p class="para mt-2"><?php echo $row['PageDescription']; ?></p><?php } ?>
+                        </div>
+
+
+                    </div>
+                    <div class="col-lg-6 ">
+                        <img src="assets/images/about-us.png" alt="product" class="img-responsive about-me">
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php include_once('includes/footer.php'); ?>
+    <!-- move top -->
+    <button onclick="topFunction()" id="movetop" title="Go to top">
+        <span class="fa fa-long-arrow-up"></span>
+    </button>
+    <script>
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {
+            scrollFunction()
+        };
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                document.getElementById("movetop").style.display = "block";
+            } else {
+                document.getElementById("movetop").style.display = "none";
+            }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
+    </script>
+    <!-- /move top -->
 </body>
 
 </html>
